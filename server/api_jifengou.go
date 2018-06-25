d8d1cdf1b4d404a271e618900543b41fec20eaf5
yaltachen <82513200@qq.com>
2018-06-25 18:30:20 +0800

    fix bug in query coupon status api

--- server/api_jifengou.go
+++ server/api_jifengou.go
@@ -28,6 +28,12 @@ type ItemData struct {
 	ItemPrice     float64 `json:"item_price"`
 }
 
+type QueryJsonCouponStatus struct {
+	Code   string `json:"code,omitempty"`
+	CardId string `json:"card_id,omitempty"`
+	SpId   int    `json:"sp_id,omitempty"`
+}
+
 func PlayGround() {
 
 }
@@ -89,9 +95,9 @@ func QueryCouponInfo(ctx *gin.Context) {
 //券码状态查询
 func QueryCouponStatus(ctx *gin.Context) {
 	crossDomain(ctx)
-
-	code := ctx.PostForm("code")
-	if code == "" {
+	var queryJson QueryJsonCouponStatus
+	ctx.ShouldBindJSON(&queryJson)
+	if queryJson.Code == "" {
 		ctx.JSON(200, &JFGResponse{
 			StatusCode: RequestFail,
 			Message:    "请求失败，缺少code参数",
@@ -100,7 +106,7 @@ func QueryCouponStatus(ctx *gin.Context) {
 		return
 	}
 
-	coupon, err := server.DB.FindCouponByCode(code)
+	coupon, err := server.DB.FindCouponByCode(queryJson.Code)
 	if err != nil {
 		sendFailedJsonResponse(ctx, RequestUrlErr)
 		return
