9240bb86883fd13666321fbb125a6bbfd73a4d2d
cuixiaotian <cxt90730@126.com>
2018-06-30 14:12:27 +0800

    add business

--- server/api_business.go
+++ server/api_business.go
@@ -3,21 +3,20 @@ package server
 import (
 	"github.com/OnebookTechnology/jifengou/server/models"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"net/http"
 )
 
 type AddBusinessReq struct {
-	BNo   string `form:"b_no"`
-	BName string `form:"b_name"`
-	BPwd  string `form:"b_pwd"`
+	BNo   string `form:"b_no" json:"b_no"`
+	BName string `form:"b_name" json:"b_name"`
+	BPwd  string `form:"b_pwd" json:"b_pwd"`
 }
 
 //添加商户
 func AddBusiness(ctx *gin.Context) {
 	crossDomain(ctx)
 	var req *AddBusinessReq
-	if ctx.ShouldBindWith(req, binding.FormPost) == nil {
+	if ctx.ShouldBindJSON(req) == nil {
 		b := &models.Business{
 			BusinessNo:           req.BNo,
 			BusinessName:         req.BName,
