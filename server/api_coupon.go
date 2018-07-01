ba096b7baccef5eab71a456216509bc2e18f14dc
cuixiaotian <cxt90730@126.com>
2018-07-01 14:36:31 +0800

    add coupon

--- server/api_coupon.go
+++ server/api_coupon.go
@@ -1,12 +1,16 @@
 package server
 
 import (
+	"github.com/OnebookTechnology/jifengou/server/models"
 	"github.com/gin-gonic/gin"
+	"strings"
 )
 
 type CouponReq struct {
 	BusinessId   int      `json:"b_id"`
 	ProductId    int      `json:"p_id"`
+	ProductStart string   `json:"p_start"`
+	ProductEnd   string   `json:"p_end"`
 	BCouponCodes []string `json:"b_codes"`
 }
 
@@ -16,14 +20,36 @@ func AddBusinessCoupon(ctx *gin.Context) {
 	var req CouponReq
 	if err := ctx.BindJSON(&req); err == nil {
 		for _, bcode := range req.BCouponCodes {
-			logger.Info(bcode)
+			//去空格
+			bcode = strings.TrimSpace(bcode)
+			if bcode == "" {
+				continue
+			}
+			logger.Info("add bcoupon:", bcode)
+			coupon := strings.Split(bcode, ",")
+			var cartId, code string
+			if len(coupon) == 1 {
+				code = coupon[0]
+			} else {
+				cartId = coupon[0]
+				code = coupon[1]
+			}
+			bc := &models.BCoupon{
+				BCCartId:     cartId,
+				BCCode:       code,
+				BId:          req.BusinessId,
+				ProductId:    req.ProductId,
+				BCStart:      req.ProductStart,
+				BCEnd:        req.ProductEnd,
+				BCStatus:     models.CouponNotReleased,
+				BCUpdateTime: nowTimestampString(),
+			}
+			err := server.DB.AddBusinessCoupon(bc)
+			if err != nil {
+				sendFailedResponse(ctx, Err, "AddBusinessCoupon err:", err)
+				return
+			}
 		}
-		//bc := &models.BCoupon{}
-		//err := server.DB.AddBusinessCoupon(bc)
-		//if err != nil {
-		//	sendFailedResponse(ctx, Err, "AddProduct err:", err)
-		//	return
-		//}
 		sendSuccessResponse(ctx, nil)
 		return
 	} else {
